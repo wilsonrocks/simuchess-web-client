1abedf13cdb8f9d6c903aa11255ee65855a18190
James Wilson <oh.that.wilson@googlemail.com>
2018-01-07 22:25:55 +0000

    added JS function to add a piece to a square and one to remove

---
+++ js/simuchess.js
@@ -0,0 +1,7 @@
+function addPiece (square, colour, piece) {
+    $("#"+square).prepend(`<img class="piece" src="images/${colour}_${piece}.svg" alt="${colour} ${piece}">`);
+}
+
+function clearSquare (square) {
+    $("#"+square+" img").remove();
+}
