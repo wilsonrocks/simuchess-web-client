4f5d3f920bdc7c12837182072c559ac0bfaaaea4
James Wilson <oh.that.wilson@googlemail.com>
2018-01-07 22:25:00 +0000

    styled chessboard

--- makeboard.py
+++ makeboard.py
@@ -2,6 +2,6 @@ print('<table class="board">')
 for chessRank in "87654321":
     print('  <tr class="row">')
     for chessFile in "abcdefgh":
-        print('    <td class="square" id="{square}">{square}</td>'.format(square=(chessRank + chessFile)))
+        print('    <td class="square" id="{square}">{square}</td>'.format(square=(chessFile + chessRank)))
     print('  </tr>')
 print('</table')
