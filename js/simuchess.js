1ece140fd137a33ecca7d801105f80dc2279d732
James Wilson <oh.that.wilson@googlemail.com>
2018-01-09 21:54:48 +0000

    set up a test login form

--- js/simuchess.js
+++ js/simuchess.js
@@ -1,3 +1,7 @@
+let user = {username: undefined,
+            password: undefined,
+            token: undefined};
+
 function addPiece (square, colour, piece) {
     $("#"+square).prepend(`<img class="piece" src="images/${colour}_${piece}.svg" alt="${colour} ${piece}">`);
 }
@@ -5,5 +9,21 @@ function addPiece (square, colour, piece) {
 function clearSquare (square) {
     $("#"+square+" img").remove();
 }
+$("#login-submit").click(function () {
+    user.username = $("#username-field").val();
+    user.password = $("#password-field").val();
+    $.getJSON("/simuchess/token",user,function(data) {
+        user.token = data.token;
+        user.passsword = undefined;
+        $(".username-greeting").text(user.username);
+        console.log(user);
+
+
+    });
+
+
+});
+
+
 
 addPiece("c6","black","pawn");
