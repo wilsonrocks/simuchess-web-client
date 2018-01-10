3cac56b234f5626f7b2eb5ad94fdb39608374f24
James Wilson <oh.that.wilson@googlemail.com>
2018-01-10 12:49:28 +0000

    uses classes to decide what is shown when logging in and not

--- js/simuchess.js
+++ js/simuchess.js
@@ -4,6 +4,8 @@ let user = {username: undefined,
             password: undefined,
             token: undefined};
 
+updatePageWithLoginStatus();
+
 function addPiece (square, colour, piece) {
     $("#"+square).prepend(`<img class="piece" src="images/${colour}_${piece}.svg" alt="${colour} ${piece}">`);
 }
@@ -12,10 +14,21 @@ function clearSquare (square) {
     $("#"+square+" img").remove();
 }
 
+function updatePageWithLoginStatus () {
+    if (user.token === undefined) { //not logged in
+        $(".logged-in").css("display","none"); //don't show the logged in stuff
+        $(".not-logged-in").css("display","unset"); //go back to whatever it was
+    }
+    else {
+        $(".logged-in").css("display","unset");
+        $(".not-logged-in").css("display","none");
+    }
+    
+}
+
 function addGameChoices () {
     $.getJSON(baseURL + "/simuchess/player", user, function (data) {
         data.games.forEach(function (gameID) {
-            console.log(gameID);
             $("#game-choice").prepend(`<option>${gameID}</option>`);
         });
     });
@@ -26,19 +39,25 @@ $("#login-submit").click(function () {
     user.password = $("#password-field").val();
     $.getJSON(baseURL + "/simuchess/token", user, function(data) {//On Success
         user.token = data.token; //manage the user object
-        user.passsword = undefined;
+        user.password = undefined;
+        $("#password-field").text("");
         
         $(".username-greeting").text(user.username);
         $("#login-error").text("").css("display","none");
-        $("#login-form").css("display","none");
+        console.log(user);       
+        updatePageWithLoginStatus();
 
         addGameChoices();
         })
         .fail(function(data) {//On failure
             console.log(data);
-            $("#login-error").text("PROBLEM:" + data.responseJSON.message);
-            $("#login-error").css("display","inline");
+            $("#login-error")
+            .text("PROBLEM:" + data.responseJSON.message)
+            .css("display", "unset");
+           
         });
+
+    
 });
 
 
