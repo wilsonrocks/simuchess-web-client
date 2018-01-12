b8e47bd20d61ff6a7c7c95325f447e527c9a749c
James Wilson <oh.that.wilson@googlemail.com>
2018-01-12 10:51:10 +0000

    Made logout function

--- js/simuchess.js
+++ js/simuchess.js
@@ -1,16 +1,17 @@
 const baseURL = "http://wilsonseverywhere.ddns.net";
 
-let user = {username: undefined,
-            password: undefined,
-            token: undefined
-        };
+let user = {}
 
+const clearUserData = function () {
+    user.username = user.password = user.token = undefined;
+}
+
+clearUserData();
 updatePageWithLoginStatus();
 
 function loggedIn() {
     return user.token !== undefined;
 }
-
 
 function addPiece (square, colour, piece) {
     $("#"+square).prepend(`<img class="piece" src="images/${colour}_${piece}.svg" alt="${colour} ${piece}">`);
@@ -20,21 +21,23 @@ function clearSquare (square) {
     $("#"+square+" img").remove();
 }
 
+function clearBoard () {
+    $(".piece").remove();
+}
+
 function updatePageWithLoginStatus () {
 
     if (loggedIn()) {
         $(".logged-in").css("display","unset");
         $(".not-logged-in").css("display","none");
+        $(".username-greeting").text(user.username);
     }
 
     else { //not logged in
         $(".logged-in").css("display","none"); //don't show the logged in stuff
         $(".not-logged-in").css("display","unset"); //go back to whatever it was
+        $(".username-greeting").text("Guest");
     }
-
-
-
-
 }
 
 function addGameChoices () {
@@ -45,6 +48,22 @@ function addGameChoices () {
     });
 }
 
+const logOut = function () {
+    $.ajax({
+        url: baseURL + "/simuchess/token",
+        type: 'DELETE',
+        data: {token:user.token},
+        success: function(request) {console.log("YO");}
+    });
+
+    clearBoard();
+    $("#game-choice").children().remove();    // wipe games list
+    clearUserData();// reset user object to undefined
+    updatePageWithLoginStatus();
+}
+
+$("#logout").click(logOut);
+
 $("#login-submit").click(function () {
     user.username = $("#username-field").val();
     user.password = $("#password-field").val();
@@ -52,10 +71,7 @@ $("#login-submit").click(function () {
         user.token = data.token; //manage the user object
         user.password = undefined;
         $("#password-field").text("");
-
-        $(".username-greeting").text(user.username);
         $("#login-error").text("").css("display","none");
-        console.log(user);
         updatePageWithLoginStatus();
 
         addGameChoices();
@@ -65,12 +81,7 @@ $("#login-submit").click(function () {
             $("#login-error")
             .text("PROBLEM:" + data.responseJSON.message)
             .css("display", "unset");
-
         });
-
-
 });
-
-
 
 addPiece("c6","black","pawn");
